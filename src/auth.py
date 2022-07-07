ddef7e684278dc09f16323bef531895ea52b7f60
Jaedson Silva <contacanal798@gmail.com>
2022-07-07 15:46:11 -0300

    removing .timestamp from exp variable

--- src/auth.py
+++ src/auth.py
@@ -16,11 +16,13 @@ def token_generate(user: dict):
     Returns:
         str: token
     """
-    exp = (datetime.datetime.now() + datetime.timedelta(days=1)).timestamp()
+
+    exp = datetime.datetime.now() + datetime.timedelta(days=1)
 
     payload = {
         key: value for key, value in user.items() if key in ('email',)
     }
+
     token_content = {'exp': exp}
     token_content.update(payload)
 
